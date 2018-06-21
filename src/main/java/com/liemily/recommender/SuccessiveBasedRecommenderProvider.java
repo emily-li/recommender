d7b7601dfb026c76d82f3d085b6ef55529791a44
emily-li <emily.li@live.co.uk>
2018-06-21 20:28:22 +0100

    Optimising recommender population

--- src/main/java/com/liemily/recommender/SuccessiveBasedRecommenderProvider.java
+++ src/main/java/com/liemily/recommender/SuccessiveBasedRecommenderProvider.java
@@ -3,7 +3,7 @@ package com.liemily.recommender;
 import com.liemily.data.Inventory;
 import com.liemily.data.UserHistory;
 
-import java.util.List;
+import java.util.*;
 
 public class SuccessiveBasedRecommenderProvider {
     public SuccessiveBasedRecommender getRecommender(Inventory inventory, UserHistory... userHistoryCollection) throws NoSuchFieldException {
@@ -11,11 +11,20 @@ public class SuccessiveBasedRecommenderProvider {
 
         final DataSet dataSet = new DataSet(itemIds, new double[itemIds.length][itemIds.length]);
         final SuccessiveBasedRecommender recommender = new SuccessiveBasedRecommender(dataSet);
+
+        Map<String, Collection<String[]>> itemSequences = new HashMap<>();
         for (UserHistory userHistory : userHistoryCollection) {
             List<String> purchaseHistory = userHistory.getPurchaseHistory();
 
             for (int i = 1; i <= purchaseHistory.size() - 1; i++) {
-                recommender.registerSuccessiveItem(purchaseHistory.get(i), purchaseHistory.subList(0, i).toArray(new String[i - 1]));
+                itemSequences.computeIfAbsent(purchaseHistory.get(i), l -> new ArrayList<>());
+                itemSequences.get(purchaseHistory.get(i)).add(purchaseHistory.subList(0, i).toArray(new String[i - 1]));
+            }
+        }
+
+        for (Map.Entry<String, Collection<String[]>> purchaseSequences : itemSequences.entrySet()) {
+            for (String[] seq : purchaseSequences.getValue()) {
+                recommender.registerSuccessiveItem(purchaseSequences.getKey(), seq);
             }
         }
         return recommender;
