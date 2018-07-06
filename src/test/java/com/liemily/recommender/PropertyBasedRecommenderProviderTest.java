0979178b2abb2f851ca0cc1b59a3e443135b7385
emily-li <emily.li@live.co.uk>
2018-07-06 12:56:53 +0100

    Adding test to check property based recommender

--- src/test/java/com/liemily/recommender/PropertyBasedRecommenderProviderTest.java
+++ src/test/java/com/liemily/recommender/PropertyBasedRecommenderProviderTest.java
@@ -1,25 +1,41 @@
-/*
 package com.liemily.recommender;
 
 import com.liemily.data.Inventory;
 import com.liemily.data.Item;
+import com.liemily.math.VectorCalculator;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PropertyBasedRecommenderProviderTest {
     private static PropertyBasedRecommenderProvider provider;
 
     @BeforeClass
     public static void setupBeforeClass() {
-        provider = new PropertyBasedRecommenderProvider();
+        provider = new PropertyBasedRecommenderProvider(new VectorCalculator());
     }
 
     @Test
-    public void testRecommenderGivenInventoryProperties() {
+    public void testRecommenderGivenInventoryProperties() throws Exception {
         Inventory inventory = new Inventory(
-                new Item("item1", new int[]{})
+                new Item("item1", new double[]{1, 1, 0}),
+                new Item("item2", new double[]{1, 0, 0}),
+                new Item("item3", new double[]{0, 0, 1})
         );
-        provider.getRecommender(inventory);
+        PropertyBasedRecommender recommender = provider.getRecommender(inventory);
+
+        DataSet actual = recommender.getDataSet();
+        double[][] expected = new double[][]{
+                new double[]{0, 0.333, 0},
+                new double[]{0.333, 0, 0},
+                new double[]{0, 0, 0}
+        };
+
+        for (int i = 0; i < inventory.getIds().length; i++) {
+            for (int j = 0; j < inventory.getIds().length; j++) {
+                assertEquals(expected[i][j], actual.get(inventory.getIds()[i], inventory.getIds()[j]), 0.0001);
+            }
+        }
     }
 }
-*/
