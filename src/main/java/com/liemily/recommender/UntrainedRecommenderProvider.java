f9c8f0bfd7006e3004ae47e21b0ef060c20ebbfe
emily-li <emily.li@live.co.uk>
2018-08-04 13:14:43 +0100

    Adding UntrainedRecommender

---
+++ src/main/java/com/liemily/recommender/UntrainedRecommenderProvider.java
@@ -0,0 +1,28 @@
+package com.liemily.recommender;
+
+import com.liemily.entity.Inventory;
+import com.liemily.entity.Item;
+
+import java.util.Random;
+
+public class UntrainedRecommenderProvider implements RecommenderProvider {
+    private final Inventory inventory;
+
+    public UntrainedRecommenderProvider(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    @Override
+    public ItemBasedRecommender getRecommender() {
+        final Item[] items = inventory.getInventory();
+        final double[][] randomSimilarities = new double[items.length][items.length];
+        final Random random = new Random();
+
+        for (int i = 0; i < items.length; i++) {
+            randomSimilarities[i] = random.doubles(items.length, 0, 1).toArray();
+        }
+
+        final DataSet dataSet = new DataSet(inventory.getIds(), randomSimilarities);
+        return new ItemBasedRecommender(dataSet);
+    }
+}
---
+++ src/test/java/com/liemily/recommender/UntrainedRecommenderProviderTest.java
@@ -0,0 +1,5 @@
+package com.liemily.recommender;
+
+public class UntrainedRecommenderProviderTest {
+
+}
